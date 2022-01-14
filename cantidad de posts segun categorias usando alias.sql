dd9789eccb28919ef12e0c58cba0694bf7bd4b34
Josué Garza <44554474+josuegarza42@users.noreply.github.com>
2022-01-14 15:02:36 -0600

    Update cantidad de posts segun categorias usando alias.sql

--- cantidad de posts segun categorias usando alias.sql
+++ cantidad de posts segun categorias usando alias.sql
@@ -3,3 +3,9 @@ from categorias as c
 inner join posts as p on c.id_categorias = p.categoria_id
 group by c.id_categorias 
 order by cant_posts desc 
+-------------------------------------------------------------------
+select u.nickname, count(*) as cant_posts
+from usuarios as u
+inner join posts as p on u.id_usuarios = p.usuario_id
+group by u.id_usuarios
+order by cant_posts desc 
