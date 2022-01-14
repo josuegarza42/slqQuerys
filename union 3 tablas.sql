0bc4370452b4d8ca344ce31ebbbadcb4c076f855
Josué Garza <44554474+josuegarza42@users.noreply.github.com>
2022-01-14 13:16:25 -0600

    Update union 3 tablas.sql

--- union 3 tablas.sql
+++ union 3 tablas.sql
@@ -3,3 +3,4 @@ from posts
 inner join post_etiquetas on posts.id_posts = post_etiquetas.post_id
 inner join etiquetas on etiquetas.id_etiquetas = post_etiquetas.etiqueta_id -- union 3 tablas, post, post etiquetas, etiquetas
 group by posts.id_posts
+order by num_etiquetas
