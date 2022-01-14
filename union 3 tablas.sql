306ff291e019fcbfa892cd62293257b0d5276737
Josué Garza <44554474+josuegarza42@users.noreply.github.com>
2022-01-14 13:15:03 -0600

    ejemplo union de 3 tablas

---
+++ union 3 tablas.sql
@@ -0,0 +1,5 @@
+select posts.titulo, count(*) as num_etiquetas
+from posts
+inner join post_etiquetas on posts.id_posts = post_etiquetas.post_id
+inner join etiquetas on etiquetas.id_etiquetas = post_etiquetas.etiqueta_id -- union 3 tablas, post, post etiquetas, etiquetas
+group by posts.id_posts
